acebfcf4b459d05f738769e24db09ced6f40db1f
Senan Qulamov <qulamovsenan@gmail.com>
2025-05-10 18:45:17 +0400

    First test commit fir java learning(repeat)

---
+++ src/day_1.java
@@ -0,0 +1,26 @@
+public class day_1{
+
+    static String randomStr = "It's me";
+    static final String randomStr_final = "Its me in str 2";
+    public static void main(String[] args){
+        System.out.println(randomStr);
+        System.out.println(randomStr_final);
+
+        int random_int_1,random_int_2,random_int_3,random_int_4,random_int_5;
+        random_int_1 = 2;
+        random_int_2 = 3;
+
+        System.out.println("random_int_1 = " + random_int_1);
+        System.out.println("random_int_2 = " + random_int_2);
+
+        System.out.println(Integer.MAX_VALUE);
+
+        char randomChar = 66;
+        char randomChar_2 = 'a';
+
+        System.out.println("randomChar = " + randomChar);
+        System.out.println("randomChar_2 = " + randomChar_2);
+
+        //learn escape characters
+    }
+}
